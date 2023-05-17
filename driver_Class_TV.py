32134504054e183280b6d746ececa3c5d30e3b13
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-05-17 22:42:49 +0800

    Incorporated pyfiglet into tkinter

--- driver_Class_TV.py
+++ driver_Class_TV.py
@@ -1,6 +1,6 @@
 from Class_TV import TV 
 import tkinter as tk
-import sys
+import pyfiglet
 class TVDriver:
     def TVTest(self):
         #Imports two TV sets
@@ -22,8 +22,10 @@ class TVDriver:
         #Print TV 1's properties
         window.title("TV Properties")
         window.minsize(300,500)
-        lbl=tk.Label(text="TV 1's channel is "+ str(first_tv.know_tv_channel()) + " and the volume is " + str(first_tv.know_tv_volume()))
-        lbl.pack()
+        title=tk.Label(text=(pyfiglet.figlet_format("Welcome!", justify="center")))
+        title.pack()
+        label=tk.Label(text="TV 1's channel is "+ str(first_tv.know_tv_channel()) + " and the volume is " + str(first_tv.know_tv_volume()))
+        label.pack()
         lbl2=tk.Label(text="TV 2's channel is " + str(second_TV.know_tv_channel()) + " and the volume is " + str(second_TV.know_tv_volume()))
         lbl2.pack()
         button=tk.Button(text="Exit", command=window.quit)
