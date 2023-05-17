d6d9b07701c1c3a9355e5aaf956895f4faf0a6dd
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-05-17 12:54:10 +0800

    Defined turning on and off

--- Class_TV.py
+++ Class_TV.py
@@ -4,3 +4,10 @@ class TV:
         tv_set.volume=volume
         tv_set.status=status
     
+    def tv_on(tv_self):
+        status=True
+    
+    def tv_off(tv_self):
+        status=False
+    
+    
