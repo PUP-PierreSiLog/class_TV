e269ae067bf20cf64441383918fb329d90335835
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-05-17 22:02:02 +0800

    Working code!

--- driver_Class_TV.py
+++ driver_Class_TV.py
@@ -12,12 +12,15 @@ class TVDriver:
 
         #Print TV 1's properties
         print("TV 1's channel is ", first_tv.know_tv_channel(), "and the volume is", first_tv.know_tv_volume())
-        
+
         #second TV's properties
-        second_TV.on()
+        second_TV.tv_on()
         second_TV.know_tv_channel()
         second_TV.tv_channel(3)
         second_TV.tv_volume(2)
+
+        #Print TV 2's properties
+        print("TV 2's channel is ", second_TV.know_tv_channel(), "and the volume is", second_TV.know_tv_volume())
 
 driver=TVDriver()
 driver.TVTest()
