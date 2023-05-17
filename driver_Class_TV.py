811330ff91a05033b5e01c5143d442151a91ccba
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-05-17 22:38:58 +0800

    Removed parentheses on window quit

--- driver_Class_TV.py
+++ driver_Class_TV.py
@@ -26,7 +26,7 @@ class TVDriver:
         lbl.pack()
         lbl2=tk.Label(text="TV 2's channel is " + str(second_TV.know_tv_channel()) + " and the volume is " + str(second_TV.know_tv_volume()))
         lbl2.pack()
-        button=tk.Button(text="Exit", command=window.quit())
+        button=tk.Button(text="Exit", command=window.quit)
         button.pack()
         window.mainloop()
 
