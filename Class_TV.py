5c1da7677c1238f59980765d1a327378e94691ea
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-05-17 13:09:24 +0800

    #Tells us what the current volume and the channel of the TV set

--- Class_TV.py
+++ Class_TV.py
@@ -4,15 +4,20 @@ class TV:
         tv_set.volume=volume
         tv_set.status=status
     
+    #Sets the condition for turning TV on
     def tv_on(tv_set):
         tv_set.status=True
-    
+    #Sets the condition for turning TV off
     def tv_off(tv_set):
         tv_set.status=False
-
+    #Sets the volume of the TV
     def tv_volume(tv_set, volume):
         tv_set.volume=volume
-
+    #Sets the channel of the TV
     def tv_channel(tv_set, channel):
         tv_set.channel=channel
-        
+    #Tells us what the current volume and the channel of the TV set
+    def know_tv_volume(tv_set):
+        return tv_set.volume
+    def know_tv_channel(tv_set):
+        return tv_set.channel
