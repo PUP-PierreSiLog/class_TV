3f0cebd60b71bbf8bbe19542543fa7b03fc18a38
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-05-17 22:00:54 +0800

    Added TV2's properties

--- driver_Class_TV.py
+++ driver_Class_TV.py
@@ -9,9 +9,15 @@ class TVDriver:
         first_tv.know_tv_channel()
         first_tv.tv_channel(30)
         first_tv.tv_volume(3)
-        
+
         #Print TV 1's properties
         print("TV 1's channel is ", first_tv.know_tv_channel(), "and the volume is", first_tv.know_tv_volume())
+        
+        #second TV's properties
+        second_TV.on()
+        second_TV.know_tv_channel()
+        second_TV.tv_channel(3)
+        second_TV.tv_volume(2)
 
 driver=TVDriver()
 driver.TVTest()
