a2a781329442d49eac1757007d49d03b51cd5f1a
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-05-17 22:18:09 +0800

    Tried putting in GUI, does not work

--- driver_Class_TV.py
+++ driver_Class_TV.py
@@ -1,4 +1,5 @@
-from Class_TV import TV
+from Class_TV import TV 
+import tkinter as tk
 class TVDriver:
     def TVTest(self):
         #Imports two TV sets
@@ -10,15 +11,16 @@ class TVDriver:
         first_tv.tv_channel(30)
         first_tv.tv_volume(3)
 
-        #Print TV 1's properties
-        print("TV 1's channel is ", first_tv.know_tv_channel(), "and the volume is", first_tv.know_tv_volume())
-
         #second TV's properties
         second_TV.tv_on()
         second_TV.know_tv_channel()
         second_TV.tv_channel(3)
         second_TV.tv_volume(2)
 
+        window=tk.Tk()
+        #Print TV 1's properties
+        lbl=tk.Label(text="TV 1's channel is "+ str(first_tv.know_tv_channel()) + "and the volume is" + str(first_tv.know_tv_volume()))
+        lbl.pack()
         #Print TV 2's properties
         print("TV 2's channel is ", second_TV.know_tv_channel(), "and the volume is", second_TV.know_tv_volume())
 
