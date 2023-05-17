7b4cb3b7ce723d3549ddd8e495945720399870f3
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-05-17 21:59:12 +0800

    Added TV Volume

--- driver_Class_TV.py
+++ driver_Class_TV.py
@@ -7,8 +7,11 @@ class TVDriver:
         #TV 1's properties
         first_tv.tv_on()
         first_tv.know_tv_channel()
-        first_tv.tv_channel(12)
+        first_tv.tv_channel(30)
+        first_tv.tv_volume(3)
+        
         #Print TV 1's properties
-        print("TV 1's channel is ", first_tv.know_tv_channel())
+        print("TV 1's channel is ", first_tv.know_tv_channel(), "and the volume is", first_tv.know_tv_volume())
+
 driver=TVDriver()
 driver.TVTest()
