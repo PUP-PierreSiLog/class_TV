30016a19a89c17df342381f1654c93b30abc684a
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-05-17 12:59:14 +0800

    Fixed classes

--- Class_TV.py
+++ Class_TV.py
@@ -1,16 +1,17 @@
 class TV:
-    def __init__(tv_set, channel=int, volume=int, status=bool):
+    def __init__(tv_set, channel=1, volume=25, status=True):
         tv_set.channel=channel
         tv_set.volume=volume
         tv_set.status=status
     
     def tv_on(tv_set):
-        status=True
+        tv_set.status=True
     
     def tv_off(tv_set):
-        status=False
+        tv_set.status=False
 
     def tv_volume(tv_set, volume):
-        volume=volume
+        tv_set.volume=volume
+        
     
     
