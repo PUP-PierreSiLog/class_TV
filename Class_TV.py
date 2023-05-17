d8d97393a031491a172fe3c8742913d66cb683df
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-05-17 12:51:03 +0800

    Defined initial attributes

--- Class_TV.py
+++ Class_TV.py
@@ -1,2 +1,6 @@
 class TV:
-    def __init__(tv_set, channel, volume, status ):
+    def __init__(tv_set, channel, volume, status):
+        tv_set.channel=channel
+        tv_set.volume=volume
+        tv_set.status=status
+    
