f09bf872179dafd8663d31275d02c424be8569bc
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-05-17 12:57:19 +0800

    Defined volume

--- Class_TV.py
+++ Class_TV.py
@@ -1,13 +1,16 @@
 class TV:
-    def __init__(tv_set, channel, volume, status):
+    def __init__(tv_set, channel=int, volume=int, status=bool):
         tv_set.channel=channel
         tv_set.volume=volume
         tv_set.status=status
     
-    def tv_on(tv_self):
+    def tv_on(tv_set):
         status=True
     
-    def tv_off(tv_self):
+    def tv_off(tv_set):
         status=False
+
+    def tv_volume(tv_set, volume):
+        volume=volume
     
     
